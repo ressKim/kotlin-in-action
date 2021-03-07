fc8cbb1bfc0772ecb3eaec78f3d99fa51f0661a0
ressKim <sh21dlat@gmail.com>
2021-03-07 23:20:51 +0900

    ext.kotlin_version change

--- src/ch02/2.3.4_WhenWithoutArument.kt
+++ src/ch02/2.3.4_WhenWithoutArument.kt
@@ -4,21 +4,21 @@ import ch02.colors.Color
 import ch02.colors.Color.*
 
 fun mixOptimized(c1: Color, c2: Color) =
-    when {
-        (c1 == RED && c2 == YELLOW) ||
-        (c1 == YELLOW && c2 == RED) ->
-            ORANGE
+        when {
+            (c1 == RED && c2 == YELLOW) ||
+                    (c1 == YELLOW && c2 == RED) ->
+                ORANGE
 
-        (c1 == YELLOW && c2 == BLUE) ||
-        (c1 == BLUE && c2 == YELLOW) ->
-            GREEN
+            (c1 == YELLOW && c2 == BLUE) ||
+                    (c1 == BLUE && c2 == YELLOW) ->
+                GREEN
 
-        (c1 == BLUE && c2 == VIOLET) ||
-        (c1 == VIOLET && c2 == BLUE) ->
-            INDIGO
+            (c1 == BLUE && c2 == VIOLET) ||
+                    (c1 == VIOLET && c2 == BLUE) ->
+                INDIGO
 
-        else -> throw Exception("Dirty color")
-    }
+            else -> throw Exception("Dirty color")
+        }
 
 fun main(args: Array<String>) {
     println(mixOptimized(BLUE, YELLOW))
